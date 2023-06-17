0dfc27adfc9f14cb4bd30f2b5e0cb09d8543b1a6
Sara <zeinabsarais27@gmail.com>
2023-06-18 01:05:52 +0530

    new change

--- routes/apiIntegration.js
+++ routes/apiIntegration.js
@@ -26,6 +26,26 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    const updatedApi = await api.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    if (!updatedApi) {
+      return res.status(404).json({ message: "API not found" });
+    }
+
+    res.json(updatedApi);
+  } catch (error) {
+    console.error("Error updating data:", error);
+    res.status(500).json({ message: "Error updating data" });
+  }
+});
+
 router.post("/", async (req, res) => {
   console.log(req.body);
 
@@ -47,7 +67,6 @@ router.post("/", async (req, res) => {
     action,
     selectedOptionContentType,
     selectedOptionRestType,
-    completeAddress,
     methodAddress,
     webServiceDescription,
 
@@ -68,12 +87,6 @@ router.post("/", async (req, res) => {
     showBasic,
   } = req.body;
 
-  // let { type } = req.body;
-
-  // if (!type) {
-  //   type = "REST"; // Set type to "REST" if it is empty
-  // }
-
   const newApi = new api({
     fname,
     selectedOption,
@@ -92,7 +105,6 @@ router.post("/", async (req, res) => {
     action,
     selectedOptionContentType,
     selectedOptionRestType,
-    completeAddress,
     methodAddress,
     webServiceDescription,
 
@@ -116,97 +128,6 @@ router.post("/", async (req, res) => {
   await newApi.save();
   console.log(newApi);
   res.send("created");
-});
-
-router.put("/:id", async (req, res) => {
-  const {
-    fname,
-    selectedOption,
-    webURI,
-    tries,
-    description,
-    webServices,
-    type,
-    basicAuthentication,
-    nameSpace,
-
-    selectedOptionSoapType,
-    webServiceName,
-    responseTime,
-    messageSoap,
-    action,
-    selectedOptionContentType,
-    selectedOptionRestType,
-    completeAddress,
-    methodAddress,
-    webServiceDescription,
-
-    parameterName,
-    parameterType,
-    elementPath,
-    dataType,
-
-    responseParameterName,
-    responseParameterType,
-    responseElementPath,
-    dataTypeResponse,
-    codeResponse,
-
-    selectedAuthentication,
-    username,
-    password,
-    showBasic,
-  } = req.body;
-
-  try {
-    const updatedApi = await api.findOneAndUpdate(
-      { _id: req.params.id }, // Find the document by ID
-      {
-        fname,
-        selectedOption,
-        webURI,
-        tries,
-        description,
-        webServices,
-        type,
-        basicAuthentication,
-        nameSpace,
-
-        selectedOptionSoapType,
-        webServiceName,
-        responseTime,
-        messageSoap,
-        action,
-        selectedOptionContentType,
-        selectedOptionRestType,
-        completeAddress,
-        methodAddress,
-        webServiceDescription,
-
-        parameterName,
-        parameterType,
-        elementPath,
-        dataType,
-
-        responseParameterName,
-        responseParameterType,
-        responseElementPath,
-        dataTypeResponse,
-        codeResponse,
-
-        selectedAuthentication,
-        username,
-        password,
-        showBasic,
-      },
-      { new: true } // Return the updated document
-    );
-
-    res.json(updatedApi);
-  } catch (error) {
-    console.error("Error updating data:", error);
-    res.status(500).json({ message: "Error updating data" });
-  }
 });
 
 module.exports = router;
